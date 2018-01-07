4ddd359b8589639b72ea88565bd03c7347e31754
Rana <alimurrazi570@gmail.com>
2018-01-07 08:45:43 +0600

    after first day

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,91 +1,378 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
     <head>
+
+        <!-- Basic -->
+        <title>SCDN</title>
+
+        <!-- Define Charset -->
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <!-- Responsive Metatag -->
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <!-- Page Description and Author -->
+        <meta name="description" content="Sulfur - Responsive HTML5 Template">
+        <meta name="author" content="Shahriyar Ahmed">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+        <!-- Bootstrap CSS  -->
+        <link rel="stylesheet" href="asset/bootstrap/css/bootstrap.min.css" type="text/css">
 
-            .full-height {
-                height: 100vh;
-            }
+        <!-- Font Awesome CSS -->
+        <link rel="stylesheet" href="asset/font-awesome/css/font-awesome.min.css" type="text/css">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+        <!-- Owl Carousel CSS -->
+        <link rel="stylesheet" href="asset/css/owl.carousel.css" type="text/css">
+        <link rel="stylesheet" href="asset/css/owl.theme.css" type="text/css">
+        <link rel="stylesheet" href="asset/css/owl.transitions.css" type="text/css">
+        
+        <!-- Css3 Transitions Styles  -->
+        <link rel="stylesheet" type="text/css" href="asset/css/animate.css">
+        
+        <!-- Lightbox CSS -->
+        <link rel="stylesheet" type="text/css" href="asset/css/lightbox.css">
 
-            .position-ref {
-                position: relative;
-            }
+        <!-- Sulfur CSS Styles  -->
+        <link rel="stylesheet" type="text/css" href="asset/css/style.css">
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+        <!-- Responsive CSS Style -->
+        <link rel="stylesheet" type="text/css" href="asset/css/responsive.css">
 
-            .content {
-                text-align: center;
-            }
 
-            .title {
-                font-size: 84px;
-            }
+        <script src="asset/js/modernizrr.js"></script>
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
+
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    <a href="{{ url('/login') }}">Login</a>
-                    <a href="{{ url('/register') }}">Register</a>
-                </div>
-            @endif
+    
+        <header class="clearfix">
+        
+        <!--
+    
+         -->
+            <!-- .container -->
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+            <!-- End Top Bar -->
+        
+            <!-- Start  Logo & Naviagtion  -->
+            <div class="navbar navbar-default navbar-top">
+                <div class="container">
+                    <div class="navbar-header">
+                        <!-- Stat Toggle Nav Link For Mobiles -->
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <i class="fa fa-bars"></i>
+                        </button>
+                        <!-- End Toggle Nav Link For Mobiles -->
+                        <a class="navbar-brand" href="index.html">SCDN</a>
+                    </div>
+                    <div class="navbar-collapse collapse">
+                        
+                        <!-- Start Navigation List -->
+                        <ul class="nav navbar-nav navbar-right">
+                            <li>
+                                <a class="active" href="index.html">Home</a>
+                            </li>
+                            <li>
+                                <a href="about.html">Gallery</a>
+                            </li>
+                            <li>
+                                <a href="service.html">Service</a>
+                            </li>
+                            <li>
+                                <a href="portfolio.html">Portfolio</a>
+                            </li>
+                            <li>
+                                <a href="blog.html">Blog</a>
+                                <ul class="dropdown">
+                                    <li>
+                                        <a href="blog-item.html">Item Page</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><a href="contact.html">Contact</a>
+                            </li>
+                        </ul>
+                        <!-- End Navigation List -->
+                    </div>
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+            </div>
+            <!-- End Header Logo & Naviagtion -->
+            
+        </header>
+        
+        
+        <!-- Start Header Section -->
+        <div class="banner">
+            <div class="overlay">
+                <div class="container">
+                    <div class="intro-text">
+                        <h1>SUST CSE Developer's Network <!-- <span>Great Sulfur</span> -->
+                        </h1>
+                        <h1><span>SCDN</span></h1>
+                    </div>
                 </div>
             </div>
         </div>
+        <!-- End Header Section -->
+        
+        
+        <!-- Start About Us Section -->
+    <section id="about-section" class="about-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-title text-center wow fadeInDown" data-wow-duration="2s" data-wow-delay="50ms">
+                        <h2>About Us</h2>
+                    </div>                        
+                </div>
+            </div>
+            <div class="row">
+               <div class="col-md-5">
+                   <div class="about-img">
+                       <img src="asset/images/corporate1.jpg" class="img-responsive" alt="About images">
+                   </div>
+               </div>
+               <div class="col-md-7">
+                   <div class="about-text">
+                       <p>Some Students of CSE-11 batch of SUST made an organization called SUST CSE Developer's Network(SCDN). The main purpose of the organization is to built a platform for the newbies cse developers. So that they can interact with each other and keep in touch with senoirs. </p>
+                   </div>
+                   
+               </div>
+                
+                
+                
+            </div>
+        </div>
+    </section>
+        
+        
+        <!-- Start Call to Action Section -->
+    <section class="call-to-action">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 wow zoomIn" data-wow-duration="2s" data-wow-delay="300ms">
+                    <p>We Help !<br> We Built !!<br>We Care !!!</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Call to Action Section -->
+        
+        
+        
+        
+        <!-- Start Team Member Section -->
+
+    <!-- End Team Member Section -->
+        
+        
+        <!-- Start Portfolio Section -->
+
+        <!-- End Portfolio Section -->
+        
+        
+        <!-- Start Service Section -->
+        <section id="service-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="section-title text-center wow fadeInDown" data-wow-duration="2s" data-wow-delay="50ms">
+                            <h2>Features</h2>
+                        <!--    <p>Contents of Our Site</p>  -->
+                        </div>                        
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="services-post">
+                    <a href="#"><i class="fa fa-tasks"></i></a>
+                    <h2>Projects</h2>
+              <p>Selected Projects of students of SUST </p>
+                </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="services-post">
+                    <a href="#"><i class="fa fa-code"></i></a>
+                    <h2>Codathon</h2>
+              <p>Questions and updates of Codathon </p>
+                </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="services-post">
+                    <a href="#"><i class="fa fa-code-fork"></i></a>
+                    <h2>Developer's List</h2>
+              <p>Top developers who are master in their field </p>
+                </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="services-post">
+                    <a href="#"><i class="fa fa-trophy"></i></a>
+                    <h2>Achievement</h2>
+              <p>Awards achieved by students of SUST </p>
+                </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="services-post">
+                    <a href="#"><i class="fa fa-picture-o"></i></a>
+                    <h2>Gallery</h2>
+              <p>A portion of our activity </p>
+                </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="services-post">
+                    <a href="#"><i class="fa fa-info-circle"></i></a>
+                    <h2>About</h2>
+              <p>Do you know more about us ? </p>
+                </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Start Service Section -->
+        
+        
+        
+        <!-- Start Testimonial Section -->
+        <section id="testimonial-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="testimonial-wrapper">
+                            <div class="testimonial-item">
+                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
+                                <img src="asset/images/team/team-1.jpg" alt="Testimonial images">
+                                <h5>John Doe</h5>
+                                <div class="desgnation">CEO, ThemeBean</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End Testimonial Section -->
+        
+        
+        <!-- Start Client Section -->
+        <div id="client-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+        <div class="section-title text-center wow fadeInDown" data-wow-duration="2s" data-wow-delay="50ms">
+                            <h2>Find Us!</h2>
+                        <!--    <p>Contents of Our Site</p>  -->
+                        </div>                        
+                                <div class="col-md-3">
+                        <a href="#"><i class="fa fa-facebook-official fa-5x" aria-hidden="true"></i></a>
+                    </div>
+                                <div class="col-md-3">
+                        <a href="#"><i class="fa fa-youtube-play fa-5x" aria-hidden="true" style="color: red"></i></a>
+                    </div>
+                                                    <div class="col-md-3">
+                        <a href="#"><i class="fa fa-twitter fa-5x" aria-hidden="true" style="color: #00aced"></i></a>
+                    </div>
+                                                    <div class="col-md-3">
+                        <a href="#"><i class="fa fa-github fa-5x" aria-hidden="true" style="color: black"></i></a>
+                    </div>
+
+
+
+<!--
+                        <div class="client-box">
+                            <ul class="client-list">
+                                <li><a href="#"><i class="fa fa-facebook-official fa-5x" aria-hidden="true"></i></a></li>
+                                <li><a href="#"><img src="asset/images/clients/client2.png" class="img-responsive" alt="Clients Logo"></a></li>
+                                <li><a href="#"><img src="asset/images/clients/client3.png" class="img-responsive" alt="Clients Logo"></a></li>
+                                <li><a href="#"><img src="asset/images/clients/client4.png" class="img-responsive" alt="Clients Logo"></a></li>
+                                <li><a href="#"><img src="asset/images/clients/client5.png" class="img-responsive" alt="Clients Logo"></a></li>
+                            </ul>
+                        </div>
+-->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Client Section -->
+        
+        
+        
+        <!-- Start Footer Section -->
+        <section id="footer-section" class="footer-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="section-heading-2">
+                            <h3 class="section-title">
+                                <span>Office Address</span>
+                            </h3>
+                        </div>
+                        
+                        <div class="footer-address">
+                            <ul>
+                                <li class="footer-contact"><i class="fa fa-home"></i>Dr.M.A. Wazed Miah IICT Building,SUST,Sylhet</li>
+                                <li class="footer-contact"><i class="fa fa-envelope"></i><a href="#">SCDN@gmail.com</a></li>
+                                <li class="footer-contact"><i class="fa fa-globe"></i><a href="#" target="_blank">www.SCDN.com</a></li>
+                            </ul>
+                        </div>
+                    </div><!--/.col-md-3 -->
+                    
+                    
+                    
+                    <div class="col-md-6">
+                        <div class="section-heading-2">
+                            <h3 class="section-title">
+                                <span>Stay With us</span>
+                            </h3>
+                        </div>
+                        <div class="subscription">
+                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.</p>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Your E-mail" id="name" required data-validation-required-message="Please enter your name.">
+                                <input type="submit" class="btn btn-primary" value="Subscribe">
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div><!--/.row -->
+            </div><!-- /.container -->
+        </section>
+        <!-- End Footer Section -->
+        
+        
+        <!-- Start CCopyright Section -->
+        <div id="copyright-section" class="copyright-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-7">
+                        <div class="copyright">
+                            Copyright © 2014. All Rights Reserved.Design and Developed by <a href="http://www.themefisher.com">Themefisher</a>
+                        </div>
+                    </div>
+                    
+                </div><!--/.row -->
+            </div><!-- /.container -->
+        </div>
+        <!-- End CCopyright Section -->
+        
+        
+        
+       <!-- Sulfur JS File -->
+        <script src="asset/js/jquery-2.1.3.min.js"></script>
+        <script src="asset/js/jquery-migrate-1.2.1.min.js"></script>
+        <script src="asset/bootstrap/js/bootstrap.min.js"></script>
+        <script src="asset/js/owl.carousel.min.js"></script>
+        <script src="asset/js/jquery.appear.js"></script>
+        <script src="asset/js/jquery.fitvids.js"></script>
+        <script src="asset/js/jquery.nicescroll.min.js"></script>
+        <script src="asset/js/lightbox.min.js"></script>
+        <script src="asset/js/count-to.js"></script>
+        <script src="asset/js/styleswitcher.js"></script>
+        
+        <script src="asset/js/map.js"></script>
+        <script src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+        <script src="asset/js/script.js"></script> 
+        
+    
     </body>
 </html>
